674fba1c3d8335fcf817a71a04bf21613de5f9dd
Joanurp <jurquijop@unal.edu.co>
2024-08-04 19:12:21 -0500

    Actualizacion

---
+++ Conection_Server_PostgreSQL.R
@@ -0,0 +1,32 @@
+library(RPostgres)
+library(DBI)
+
+# Crear la conexión utilizando RPostgres
+conexion <- dbConnect(RPostgres::Postgres(), dbname = "CTrabajo",
+                      host = "localhost",
+                      port = 5432,
+                      user = "postgres",
+                      password = "...")
+
+
+estructura <- dbGetQuery(conexion, "
+  SELECT column_name
+  FROM information_schema.columns
+  WHERE table_name = 'Base1';")
+
+
+# Proporcion de medalllas
+query <- "
+  SELECT medal, 
+         COUNT(*) * 100.0 / (SELECT COUNT(*) FROM \"Base1\") AS porcentaje
+  FROM \"Base1\"
+  GROUP BY medal;
+"
+
+# Promedio
+query <- "SELECT AVG(medal) AS promedio_Base FROM \"Base1\";"
+resultado <- dbGetQuery(conexion, query)
+
+print(resultado)
+
+
